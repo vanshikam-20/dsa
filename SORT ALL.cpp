a9519b032f250b3e59debce50a8ecc71a53334ae
vanshikam-20 <vanshikam0101@gmail.com>
2024-07-24 18:44:04 +0530

    Add files via upload

---
+++ SORT ALL.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+void selection_sort(int arr[],int n){
+	for(int i=0;i<=n-2;i++){
+	int	mini=i;
+		for(int j=i;j<=n-1;j++){
+			if(arr[j]<arr[mini]){
+				mini=j;
+			}
+		}
+		int temp=arr[mini];
+		arr[mini]=arr[i];
+		arr[i]=temp;
+	}
+}
+
+void bubble_sort(int arr[],int n){
+	for(int i=n-1;i>=0;i--){
+		for(int j=0;j<=i-1;j++){
+			if(arr[j]>arr[j+1]){
+				int temp = arr[j+1];
+				arr[j+1]=arr[j];
+				arr[j]=temp;
+			}
+		}
+	}
+}
+
+void insertion_sort(int arr[],int n){
+	for (int i=0;i<=n-1;i++){
+		int j=i;
+		while(j>0 && arr[j-1]>arr[j]){
+			int temp =arr[j-1];
+			arr[j-1]=arr[j];
+			arr[j]=temp;
+			
+			j--;
+		}
+	}
+}
+
+int main(){
+	int n;
+	int arr[n];
+	cout<<"enter tehe size of arr";
+	cin>>n;
+	cout<<"enter the elemnets of arr";
+	for(int i=0;i<n;i++) cin>>arr[i];
+	insertion_sort(arr,n);
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<"\t";
+	}
+	return 0;
+}
+
